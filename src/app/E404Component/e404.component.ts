1ddf21b70d39cf6677671ce0c40ea883ff82f366
Harold Espinoza <haroldesptru@gmail.com>
2019-02-25 15:38:13 -0600

    Añadido: componente a mostrar en los errores 404.

---
+++ src/app/E404Component/e404.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'error-404',
+  template: `
+    <h1>Error 404</h1>
+    <h2>Page not found</h2>
+  `
+})
+
+export class E404Component{}
